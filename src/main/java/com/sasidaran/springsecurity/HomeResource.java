0080028cab9335e8a46179938afc205e2b468555
sasidaran_lister <sasidaran.rajaram@listertechnologies.com>
2019-10-12 16:30:10 +0530

    first commit

---
+++ src/main/java/com/sasidaran/springsecurity/HomeResource.java
@@ -0,0 +1,28 @@
+package com.sasidaran.springsecurity;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.http.MediaType;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@RestController
+public class HomeResource {
+
+    @GetMapping(value = "/")
+    public String getWelcomeText() {
+        return ("<html><body><img src = 'http://google.co.in'/></body></html>");
+    }
+
+    @GetMapping(value = "/image", produces = MediaType.TEXT_PLAIN_VALUE)
+    public byte[] getImage() throws IOException {
+        File file = ResourceUtils.getFile("classpath:img/image2.jpg");
+        InputStream in = new FileInputStream(file);
+        return IOUtils.toByteArray(in);
+    }
+}
